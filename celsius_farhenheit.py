a1125727d5d3649bad26570ac8c9812270a39f1d
Gokul58 <60250414+Gokul58@users.noreply.github.com>
2020-01-24 11:46:37 +0530

    Add files via upload

---
+++ celsius_farhenheit.py
@@ -0,0 +1,6 @@
+a = int(input('enter the celsius '))
+b = int(input('enter the farhenheit '))
+f = 9/5*a+32
+c = (160-5*b)/9
+print('farhenheit to celsius is ',f)
+print('celsius to farhenheit is ',c)
